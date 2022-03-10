5005402d8b99c1fc06e468ee60c874dcb099ce53
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-10 09:51:43 +0500

    Update resto.service.ts

--- src/app/shared/resto.service.ts
+++ src/app/shared/resto.service.ts
@@ -1,30 +1,29 @@
 import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RestoService {
-url = "http://localhost:3000/resturents"
-registerUrl = "http://localhost:3000/users"
+  url = 'http://localhost:3000/resturents';
+  registerUrl = 'http://localhost:3000/users';
 
-  constructor( private http:HttpClient) { }
-getList(){
-return this.http.get(this.url);
-}
-saveResto(data:any) {
-
-  return this.http.post(this.url , data)
-}
-DeleteResto(id: any){
-  return this.http.delete(`${this.url}/${id}`);
-}
-getCurrentResto(id:any){
-  return this.http.get(`${this.url}/${id}`);
-}
-postCurrent(id:any , data:any){
-  return this.http.put(`${this.url}/${id}`, data);
-}
- register(data : any){
-return this.http.post(this.registerUrl ,data)
- }
+  constructor(private http: HttpClient) {}
+  getList() {
+    return this.http.get(this.url);
+  }
+  saveResto(data: any) {
+    return this.http.post(this.url, data);
+  }
+  DeleteResto(id: any) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  getCurrentResto(id: any) {
+    return this.http.get(`${this.url}/${id}`);
+  }
+  postCurrent(id: any, data: any) {
+    return this.http.put(`${this.url}/${id}`, data);
+  }
+  register(data: any) {
+    return this.http.post(this.registerUrl, data);
+  }
 }
