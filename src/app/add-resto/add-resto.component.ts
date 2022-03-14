d40a63b1ded5d543417494ae007de80117b51bd5
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-14 11:31:52 -0700

    Update add-resto.component.ts

--- src/app/add-resto/add-resto.component.ts
+++ src/app/add-resto/add-resto.component.ts
@@ -1,29 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup , FormControl } from '@angular/forms';
-import {RestoService} from '../shared/resto.service'
+import { FormGroup, FormControl } from '@angular/forms';
+import { RestoService } from '../shared/resto.service';
 @Component({
   selector: 'app-add-resto',
   templateUrl: './add-resto.component.html',
-  styleUrls: ['./add-resto.component.css']
+  styleUrls: ['./add-resto.component.css'],
 })
 export class AddRestoComponent implements OnInit {
-  alert : boolean = false;
-addResto = new FormGroup({
-name : new FormControl(''),
-email : new FormControl(''),
-address : new FormControl(''),
-})
-  constructor(private resto : RestoService) { }
-collectResto(){                                         
-this.resto.saveResto(this.addResto.value).subscribe((result)=> {
-this.alert = true;
-this.resto.getList();
-});
-this.addResto.reset();
-}
-  ngOnInit(): void {
+  alert: boolean = false;
+  addResto = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    address: new FormControl(''),
+  });
+  constructor(private resto: RestoService) {}
+  collectResto() {
+    this.resto.saveResto(this.addResto.value).subscribe((result) => {
+      this.alert = true;
+      this.resto.getList();
+    });
+    this.addResto.reset();
   }
-closeAlert(){
-this.alert=false;
-}
+  ngOnInit(): void {}
+  closeAlert() {
+    this.alert = false;
+  }
 }
