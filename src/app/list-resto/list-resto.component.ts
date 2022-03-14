bab56b011ce008fcaa805dc49f5a699ecdaa1f4e
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-14 11:30:53 -0700

    Update list-resto.component.ts

--- src/app/list-resto/list-resto.component.ts
+++ src/app/list-resto/list-resto.component.ts
@@ -1,33 +1,57 @@
-import { Component, OnInit , NgIterable } from '@angular/core';
+import { Component, OnInit, NgIterable } from '@angular/core';
 import { RestoService } from '../shared/resto.service';
-
+import * as _ from 'lodash'
 @Component({
   selector: 'app-list-resto',
   templateUrl: './list-resto.component.html',
   styleUrls: ['./list-resto.component.css']
 })
 export class ListRestoComponent implements OnInit {
- 
+  name: string;
+  searchValue: any;
 
-collection:any = [
-
-];
-
-  constructor(private resto : RestoService) { }
+  collection: any = [];
+sourceFile :any = [];
+  constructor(private resto: RestoService) { }
 
   ngOnInit(): void {
-this.resto.getList().subscribe((result) => {
+    this.resto.getList().subscribe((result) => {
 
-  this.collection=result;
+      this.collection = result;
 
-})}
+    })
+  }
 
-deleteResto(list:any) {
-  console.warn(list)
-this.resto.DeleteResto(list).subscribe((result)=>{
-this.collection.splice(list-1 , 1)
-})
-}
+  deleteResto(list: any) {
+    console.warn(list)
+    this.resto.DeleteResto(list).subscribe((result) => {
+      this.collection.splice(list - 1, 1)
+    })
+  }
+//   filterevent(event : Event){
+//     this.searchValue = (event.target as HTMLInputElement).value;
+// if(!this.searchValue) {
+// this.sourceFile = _.cloneDeep(this.collection);
+// return;
+
+// }
+// console.log(this.sourceFile)
+// this.sourceFile = this.collection.filter((list:any) => {
+// return Object.keys(list).some((key) => {
+// return String (list[key]).toLowerCase().includes(this.searchValue.toLowerCase());
+// })
+// })
+//   }
+// Search(){
+//   this.collection = this.collection.filter((res : string ) => {
+// return res..toLowerCase().match(this.collection.name.toLowerCase());
+
+//   })
+
+  
+// }
+
+
 }
 
 
