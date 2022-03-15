79172057cdedfaa64f0318ebcdd45c08f91e5433
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-15 15:26:40 +0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'update/:id', component: UpdateRestoComponent },
   { path: 'update', component: UpdateRestoComponent },
-  { path: '', component: UpdateRestoComponent },
+  { path: '', component: ListRestoComponent },
   { path: 'full-detailed', component: FullDetailedComponent },
 ];
 
