d4693397aa036aa225b70ad061bc8085671bbf5f
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-15 10:09:58 +0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRestoComponent } from './add-resto/add-resto.component';
+import { FullDetailedComponent } from './full-detailed/full-detailed.component';
 import { ListRestoComponent } from './list-resto/list-resto.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UpdateRestoComponent } from './update-resto/update-resto.component';
 
 const routes: Routes = [
-
-{path : 'add' , component : AddRestoComponent},
-{path : 'list' , component : ListRestoComponent},
-{path : 'login' , component : LoginComponent},
-{path : 'register' , component : RegisterComponent},
-{path : 'update/:id', component : UpdateRestoComponent},
-{path : 'update', component : UpdateRestoComponent},
-{path : '', component : UpdateRestoComponent},
-
-
+  { path: 'add', component: AddRestoComponent },
+  { path: 'list', component: ListRestoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'update/:id', component: UpdateRestoComponent },
+  { path: 'update', component: UpdateRestoComponent },
+  { path: '', component: UpdateRestoComponent },
+  { path: 'full-detailed', component: FullDetailedComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
