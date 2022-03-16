b9e57bd50d04c516b274b269db360fcc0690e7d5
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-16 12:16:23 +0500

    Update update-resto.component.ts

--- src/app/update-resto/update-resto.component.ts
+++ src/app/update-resto/update-resto.component.ts
@@ -5,33 +5,39 @@ import { RestoService } from '../shared/resto.service';
 @Component({
   selector: 'app-update-resto',
   templateUrl: './update-resto.component.html',
-  styleUrls: ['./update-resto.component.css']
+  styleUrls: ['./update-resto.component.css'],
 })
 export class UpdateRestoComponent implements OnInit {
   alert: boolean = false;
   [x: string]: any;
   editResto = new FormGroup({
+    //hum ney form declar kiya formGroup = editResto k naam sey
     name: new FormControl(''),
     email: new FormControl(''),
     address: new FormControl(''),
-  })
-  restoData: any = {};
-  constructor(private router: ActivatedRoute, private resto: RestoService) { }
+  });
+  restoData: any = {}; // ek hum ney new json banayiee hai restoData k naam sey
+  constructor(private router: ActivatedRoute, private resto: RestoService) {}
   ngOnInit(): void {
-    // console.warn(this.router.snapshot.params['id'])
-    this.resto.getCurrentResto(this.router.snapshot.params['id']).subscribe((result) => {
-      this.restoData = result;
-      this.editResto = new FormGroup({
-        name: new FormControl(this.restoData.name),
-        email: new FormControl(this.restoData.email),
-        address: new FormControl(this.restoData.address),
-      })
-    })
+    // OnInit k mtlb jo cheezen hamey intially chaiyen sab sey pehley
+    this.resto
+      .getCurrentResto(this.router.snapshot.params['id']) // q k hum ney routing krtey waqt id phenki list-resto mai to wohi service wala function call kiya aur us k parameter mai hum ney id phenk di
+      .subscribe((result) => {
+        this.restoData = result; // jo data hamarey paas list sey aa raha hai us ko service k equal krtey huwey ..
+        this.editResto = new FormGroup({
+          // update wala form jis mai ku6 nh hai us ko liya aur us k formControl mai jo data list-resto sey aa raha hai us k eual kr diya ..!
+          name: new FormControl(this.restoData.name),
+          email: new FormControl(this.restoData.email),
+          address: new FormControl(this.restoData.address),
+        });
+      });
   }
   getCollection() {
-    this.resto.postCurrent(this.router.snapshot.params['id'], this.editResto.value).subscribe((result) => {
-      this.alert = true;
-    })
+    this.resto
+      .postCurrent(this.router.snapshot.params['id'], this.editResto.value)
+      .subscribe((result) => {
+        this.alert = true;
+      });
   }
   closeAlert() {
     this.alert = false;
