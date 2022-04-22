ed1091c1a215f8b42c6d72600b0c9412137f2158
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-04-22 10:35:26 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddRestoComponent } from './add-resto/add-resto.component';
+import { DatePiComponent } from './date-pi/date-pi.component';
 import { FullDetailedComponent } from './full-detailed/full-detailed.component';
 import { ListRestoComponent } from './list-resto/list-resto.component';
 import { LoginComponent } from './login/login.component';
@@ -9,12 +10,14 @@ import { UpdateRestoComponent } from './update-resto/update-resto.component';
 
 const routes: Routes = [
   { path: 'add', component: AddRestoComponent },
+  { path: 'date', component: DatePiComponent },
+
   { path: 'list', component: ListRestoComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'update/:id', component: UpdateRestoComponent },
   { path: 'update', component: UpdateRestoComponent },
-  { path: '', component: ListRestoComponent },
+  { path: '', component: DatePiComponent },
   { path: 'full-detailed', component: FullDetailedComponent },
 ];
 
